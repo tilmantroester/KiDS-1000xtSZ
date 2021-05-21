Updated to correct ell-binning, fixed bug in cel2gal (flip e1 sign), added cosmic shear likelihood

scripts/compute_KiDS_stats.py
@@ -11,6 +11,19 @@ sys.path.append("../tools/")
 from misc_utils import file_header, read_partial_map
 
 
+def transform_cel_to_gal(ra, dec, e1, e2):
+    r = healpy.Rotator(coord=["C", "G"])
+    l, b = r(ra, dec, lonlat=True)
+    alpha = r.angle_ref(ra, dec, lonlat=True)
+    e = np.sqrt(e1**2 + e2**2)
+    # Need to switch sign of e1 here
+    phi = np.arctan2(e2, -e1)/2
+    e1_gal = e*np.cos(2*(phi+alpha))
+    e2_gal = e*np.sin(2*(phi+alpha))
+
+    return l, b, e1_gal, e2_gal
+
+
 if __name__ == "__main__":
     Z_CUTS = [(0.1, 0.3),
               (0.3, 0.5),
@@ -31,7 +44,7 @@ if __name__ == "__main__":
     nside = 2048
 
     if catalog_stats:
-        patches = ["N", "S", "All"]
+        patches = ["All", "N", "S"]
 
         print("Catalog-based stats")
 
@@ -83,12 +96,10 @@ if __name__ == "__main__":
                 if create_compressed_catalogs:
                     catalog_name = f"KiDS-1000_{patch}_z{z_cut[0]}-{z_cut[1]}"
                     if convert_to_gal:
-                        l, b, e1_gal, e2_gal = \
-                            pylenspice.convert_shear_to_galactic_coordinates(
-                                ra, dec, e1, e2)
+                        l, b, e1_gal, e2_gal = transform_cel_to_gal(
+                                                        ra, dec, e1, e2)
                         pixel_idx_gal = healpy.ang2pix(nside,
-                                                       -b/180*np.pi+np.pi/2,
-                                                       l/180*np.pi)
+                                                       l, b, lonlat=True)
                         filename = os.path.join(compressed_catalog_path,
                                                 catalog_name+"_galactic")
                         np.savez(filename,
@@ -96,8 +107,7 @@ if __name__ == "__main__":
                                  pixel_idx=pixel_idx_gal)
                     
                     pixel_idx = healpy.ang2pix(nside,
-                                               -dec/180*np.pi+np.pi/2,
-                                               ra/180*np.pi)
+                                               ra, dec, lonlat=True)
                     filename = os.path.join(compressed_catalog_path,
                                             catalog_name)
                     np.savez(filename, ra=ra, dec=dec, w=w, e1=e1, e2=e2,

scripts/namaster_cosmic_shear_measurements.py
@@ -167,6 +167,9 @@ if __name__ == "__main__":
     parser.add_argument("--compute-coupling-matrix", action="store_true")
     parser.add_argument("--binary-mask", action="store_true")
 
+    parser.add_argument("--no-flip-e1", action="store_true")
+    parser.add_argument("--flip-e2", action="store_true")
+
     args = parser.parse_args()
 
     nside = 2048
@@ -194,6 +197,18 @@ if __name__ == "__main__":
     binary_mask = args.binary_mask
     if binary_mask:
         print("Using binary mask")
+
+    flip_e1 = not args.no_flip_e1
+    if flip_e1:
+        print("Flipping sign of e1.")
+    else:
+        print("Not flipping sign of e1.")
+
+    flip_e2 = args.flip_e2
+    if flip_e2:
+        print("Flipping sign of e2.")
+    else:
+        print("Not flipping sign of e2.")
 
     # Creating binning object
     if args.bin_operator.find("delta_ell_") == 0:
@@ -296,14 +311,18 @@ if __name__ == "__main__":
         good_pixel_map = {}
         mean_w2_sigma2 = {}
         field = {}
+
+        e1_sign = -1 if flip_e1 else 1
+        e2_sign = -1 if flip_e2 else 1
+
         for probe, shear_catalog_file in zip(probes, args.shear_catalogs):
             print("Probe ", probe)
             print("  Loading shear catalog: ", shear_catalog_file)
             shear_data[probe] = np.load(shear_catalog_file)
             e1_map, e2_map, w_map[probe], w2_s2_map = make_maps(
                                                 nside,
-                                                -shear_data[probe]["e1"],
-                                                shear_data[probe]["e2"],
+                                                e1_sign*shear_data[probe]["e1"],
+                                                e2_sign*shear_data[probe]["e2"],
                                                 shear_data[probe]["w"],
                                                 shear_data[probe]["pixel_idx"],
                                                 return_w2_sigma2=True)
@@ -420,8 +439,8 @@ if __name__ == "__main__":
             else:
                 random_e1_map, random_e2_map, _ = \
                                     make_maps(nside,
-                                              -shear_data[probe]["e1"],
-                                              shear_data[probe]["e2"],
+                                              e1_sign*shear_data[probe]["e1"],
+                                              e2_sign*shear_data[probe]["e2"],
                                               shear_data[probe]["w"],
                                               shear_data[probe]["pixel_idx"],
                                               rotate=True)