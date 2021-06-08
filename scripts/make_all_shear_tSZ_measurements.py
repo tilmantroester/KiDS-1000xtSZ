b8455cfe3f372f5cfcbf0c9c66294e9b17a80983
Tilman Troester <tilman.troester@gmail.com>
2021-06-08 15:48:07 +0100

    ACT measurements

--- scripts/make_all_shear_tSZ_measurements.py
+++ scripts/make_all_shear_tSZ_measurements.py
@@ -7,18 +7,37 @@ if __name__ == "__main__":
 
     bin_operator_file = "../data/xcorr/bin_operator_log_n_bin_12_ell_51-2952_namaster.txt"      # noqa: E501
 
-    workspace_file_template = ("/disk09/ttroester/project_triad/namaster_workspaces/"  # noqa: E501
-                               "shear_KiDS1000_y_milca/"
-                               "pymaster_workspace_shear_{}_y_{}.fits")
+    workspace_file_template = ("/disk09/ttroester/project_triad/namaster_workspaces/"                    # noqa: E501
+                               "shear_KiDS1000_y_milca/pymaster_workspace_shear_{}_foreground_{}.fits")  # noqa: E501
 
     bandpower_window_file_template = ("../results/measurements/shear_KiDS1000_y_milca/"  # noqa: E501
                                       "data/pymaster_bandpower_windows_{}-{}.npy")              # noqa: E501
 
-    Cl_data_file_template = ("../results/measurements/shear_KiDS1000_y_milca/"           # noqa: E501
+    # Planck milca
+    # Cl_data_file_template = ("../results/measurements/shear_KiDS1000_y_milca/"           # noqa: E501
+    #                          "data/Cl_gal_{}-{}.npz")
+    # Cl_cov_file_template = ("../results/measurements/shear_KiDS1000_y_milca/"            # noqa: E501
+    #                         "cov_Cls/Cl_cov_CCL_gal_{}-{}.npz")
+    # Planck nilc
+    Cl_data_file_template = ("../results/measurements/shear_KiDS1000_y_nilc/"           # noqa: E501
                              "data/Cl_gal_{}-{}.npz")
-
-    Cl_cov_file_template = ("../results/measurements/shear_KiDS1000_y_milca/"            # noqa: E501
-                            "cov_Cls/Cl_cov_CCL_gal_{}-{}.npz")
+    Cl_cov_file_template = ("../results/measurements/shear_KiDS1000_y_nilc/"            # noqa: E501
+                            "cov_Cls/Cl_cov_{}-{}.npz")
+    # Ziang's CIB deprojected map
+    # Cl_data_file_template = ("../results/measurements/shear_KiDS1000_y_ziang_nocib/"           # noqa: E501
+    #                          "data/Cl_gal_{}-{}.npz")
+    # Cl_cov_file_template = ("../results/measurements/shear_KiDS1000_y_ziang_nocib/"            # noqa: E501
+    #                         "cov_Cls/Cl_cov_CCL_gal_{}-{}.npz")
+    # ACT BN
+    # Cl_data_file_template = ("../results/measurements/shear_KiDS1000_cel_y_ACT_BN/"           # noqa: E501
+    #                          "data/Cl_cel_{}-{}.npz")
+    # Cl_cov_file_template = ("../results/measurements/shear_KiDS1000_cel_y_ACT_BN/"            # noqa: E501
+    #                         "cov_Cls/Cl_cov_{}-{}.npz")
+    # ACT BN nocib
+    # Cl_data_file_template = ("../results/measurements/shear_KiDS1000_cel_y_ACT_BN_nocib/"           # noqa: E501
+    #                          "data/Cl_cel_{}-{}.npz")
+    # Cl_cov_file_template = ("../results/measurements/shear_KiDS1000_cel_y_ACT_BN_nocib/"            # noqa: E501
+    #                         "cov_Cls/Cl_cov_{}-{}.npz")
 
     catalog_files = ["../data/shear_catalogs_KiDS1000/KiDS-1000_All_z0.1-0.3_galactic.npz",     # noqa: E501
                      "../data/shear_catalogs_KiDS1000/KiDS-1000_All_z0.3-0.5_galactic.npz",     # noqa: E501
@@ -33,8 +52,21 @@ if __name__ == "__main__":
                          "../data/shear_catalogs_KiDS1000/KiDS-1000_All_z0.7-0.9.npz",     # noqa: E501
                          "../data/shear_catalogs_KiDS1000/KiDS-1000_All_z0.9-1.2.npz"]     # noqa: E501
 
-    foreground_map = "../data/y_maps/polspice/milca/triplet.fits"
-    foreground_mask = "../data/y_maps/polspice/milca/singlet_mask.fits"
+    # Planck milca
+    # foreground_map = "../data/y_maps/polspice/milca/triplet.fits"
+    # foreground_mask = "../data/y_maps/polspice/milca/singlet_mask.fits"
+    # Planck nilc
+    foreground_map = "../data/y_maps/Planck_processed/nilc_full.fits"
+    foreground_mask = "../data/y_maps/Planck_processed/mask_ps_gal40.fits"
+    # Ziang's CIB deprojected map
+    # foreground_map = "../data/y_maps/Planck_processed/ziang/ymap_rawcov_needlet_galmasked_v1.02_bp.fits"  # noqa: E501
+    # foreground_mask = "../data/y_maps/polspice/milca/singlet_mask.fits"
+    # ACT BN
+    # foreground_map = "../data/y_maps/ACT/BN.fits"
+    # foreground_mask = "../data/y_maps/ACT/BN_planck_ps_gal40_mask.fits"
+    # ACT BN nocib
+    # foreground_map = "../data/y_maps/ACT/BN_deproject_cib.fits"
+    # foreground_mask = "../data/y_maps/ACT/BN_planck_ps_gal40_mask.fits"
 
     raw_ell_file = "../runs/cov_theory_predictions_run0/output/data_block/shear_y_cl_beam_pixwin/ell.txt"  # noqa: E501
 
@@ -79,7 +111,8 @@ if __name__ == "__main__":
         cmd += ["--Cl-data-filename", Cl_data_file]
 
         bandpower_window_file = bandpower_window_file_template.format(*idx)
-        cmd += ["--bandpower-windows-filename", bandpower_window_file]
+        if not os.path.isfile(bandpower_window_file):
+            cmd += ["--bandpower-windows-filename", bandpower_window_file]
 
         if galactic_coordinates:
             cmd += ["--no-flip-e1"]
